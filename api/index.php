83adfff8da322bb939fc3974a59edc01751fc053
amg1551 <81032941+amg1551@users.noreply.github.com>
2023-08-30 18:40:28 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,11 +4,7 @@ echo "whats happening";
 $haystack = "How are you";
 $needle   = "are";
 
-if ($needle != '' && str_contains($haystack, $needle)) {
-    echo "This returned true!";
-} else {
-    echo "This returned false!";
-}
+
 
     echo "tamamlandı";
 ?>
